f5fe742dbee482c1c520b6dab04fae90c51fdf01
tony <trung.bui26@gmail.com>
2025-08-22 11:13:35 +1000

    remove unneccessary parts for the subset app

--- app/api/auth/[...nextauth]/route.js
+++
@@ -1,6 +0,0 @@
-import { NextAuthOptions } from "@/lib/auth/nextAuthOptions";
-
-import NextAuth from "next-auth/next";
-
-const handler = NextAuth(NextAuthOptions);
-export { handler as POST, handler as GET };
