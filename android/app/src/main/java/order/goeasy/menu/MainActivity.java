56d552897224b841a486d42cdda298dcd5e1efdf
tony <trung.bui26@gmail.com>
2025-08-25 22:49:05 +1000

    added capacitor

---
+++ android/app/src/main/java/order/goeasy/menu/MainActivity.java
@@ -0,0 +1,5 @@
+package order.goeasy.menu;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "order.goeasy.menu",
+  appName: "order-manager-by-goeasymenu",
+  webDir: "public",
+  server: {
+    url: "http://localhost:3001",
+    cleartext: true,
+  },
+};
+
+export default config;
