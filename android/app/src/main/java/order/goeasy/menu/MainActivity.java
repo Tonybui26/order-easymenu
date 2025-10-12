0c7e7ef31af9dfddfe305c769d035c751cfcf476
tony <trung.bui26@gmail.com>
2025-10-12 12:51:18 +1100

    fix building android capacitor app sign abb

--- android/app/src/main/java/order/goeasy/menu/MainActivity.java
+++ android/app/src/main/java/order/goeasy/menu/MainActivity.java
@@ -2,15 +2,11 @@ package order.goeasy.menu;
 
 import com.getcapacitor.BridgeActivity;
 
-// Import our custom plugin
-import com.yourapp.printertcpsocket.PrinterTcpSocketPlugin;
-
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Register our custom plugin
-        registerPlugin(PrinterTcpSocketPlugin.class);
+        // Capacitor auto-registers plugins from capacitor.settings.gradle
     }
 }
